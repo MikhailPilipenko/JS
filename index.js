abcef510eb148bddd540229537a619c1150c4611
Pavel <pl.kozhemyakin@gmail.com>
2019-10-14 19:15:27 +0300

    14,10,2019
    йцук

---
+++ index.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+    id: 1,
+    name: 'Jone',
+    surName: 'Smith',
+    img: '',
+    age: 18,
+    gender: 'male'
+    },{
+    id: 2,
+    name: 'Adolf',
+    surName: 'Stalin',
+    img: '',
+    age: 88,
+    gender: 'male'
+    },{
+    id: 3,
+    name: 'Iosif',
+    surName: 'Gitler',
+    img: '',
+    age: 8,
+    gender: 'male'
+    },{
+    id: 4,
+    name: 'Yoko',
+    surName: 'Ono',
+    img: '',
+    age: 28,
+    gender: 'female'
+    },{
+    id: 5,
+    name: 'Lola',
+    surName: 'Begi',
+    img: '',
+    age: 24,
+    gender: 'female'
+    },{
+    id: 6,
+    name: 'Garik',
+    surName: 'White',
+    img: '',
+    age: 33,
+    gender: 'male'
+    }
+]
+
+for(let i=0; i<users.length; i++)
+    {
+        console.log(users[i]);
+//        console.log(users[i].age);
+    }
