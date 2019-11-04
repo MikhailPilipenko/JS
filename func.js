d33ee0be4ee7b972c9df646abe2f265f4b8ab163
Mikhail <pl.kozhemyakin@gmail.com>
2019-11-04 21:14:01 +0300

    04112019

--- func.js
+++ func.js
@@ -62,77 +62,168 @@
 // .then(function(a){
 //       console.log(a)
 //       })
+//__________________________________________________________________
+//  function httpGet(url){
+//   return new Promise(function(resolve, reject){
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
 
- function httpGet(url){
-  return new Promise(function(resolve, reject){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-
-    xhr.onload = function(){
-      if (this.status == 200){
-        resolve(this.response)
-      }else{
-        let error = new Error(this.statusText);
-        error.code = this.status;
-        reject(error);
-      }
-    };
+//     xhr.onload = function(){
+//       if (this.status == 200){
+//         resolve(this.response)
+//       }else{
+//         let error = new Error(this.statusText);
+//         error.code = this.status;
+//         reject(error);
+//       }
+//     };
     
-    xhr.onerror = function() {
-      reject(new Error("Network Error"));
-    };
-    xhr.send();
-  });
+//     xhr.onerror = function() {
+//       reject(new Error("Network Error"));
+//     };
+//     xhr.send();
+//   });
 
- }
+//  }
 
- let a = httpGet('https://randomuser.me/api/');
- a
- .then(
-  response => alert(`Fulfilled: ${response}`),
-  error => alert(`Rejected: ${error}`)
-);
+//  let a = httpGet('https://randomuser.me/api/');
+//  a
+//  .then(
+//   response => alert(`Fulfilled: ${response}`),
+//   error => alert(`Rejected: ${error}`)
+// );
 
-const usersArr = [];
-for(let i = 0; i<20; i++){
-  httpGet('https://randomuser.me/api/')
-    .then(function(res){
-      let goodFormat = JSON.parse(res);
-      // console.log(goodFormat)
-      usersArr.push(goodFormat)
+// const usersArr = [];
+// for(let i = 0; i<20; i++){
+//   httpGet('https://randomuser.me/api/')
+//     .then(function(res){
+//       let goodFormat = JSON.parse(res);
+//       // console.log(goodFormat)
+//       usersArr.push(goodFormat)
+//     }
+//   )
+// }
+
+// console.log(usersArr);
+
+// function viewList(data) {
+//   // контейнер для сбора елементов что бы вставлять оин раз
+//   let appendElem = document.createDocumentFragment();
+ 
+//   data.forEach(function(item){
+     
+//      let card = document.createElement("div")
+//       card.classList.add('wrap')
+     
+//       let template = `
+//           <img class="avatar" src=${item.img}>
+//           <div class="content">
+//               <h2>name: ${item.name}</h2>
+//               <ul>
+//                   <li>surname: ${item.surName}</li>
+//                   <li>age:${item.age}</li>
+//                   <li>gender:${item.gender}</li>
+//               </ul>
+//           </div>
+ 
+//       `
+//   card.innerHTML = template
+//   appendElem.appendChild(card)
+//       })
+ 
+//   document.body.append(appendElem)
+
+// }
+
+// viewList(usersArr);s
+//_______________________________________________________________________
+// function a(){
+//   console.log(this);
+// }
+
+
+// function hello() {
+//   console.log(this.name + this.surname)
+// }
+
+const user = {
+  name:"Igor",
+  surname:"Smith",
+  }
+
+  const user2 = {
+    name:"Igor2",
+    surname:"Smith2",
     }
-  )
+//--------------------------------------------111111
+// user.sayHello = function(){
+//   console.log('Привет я ' + this.name + this.surname)
+// };
+
+// user2.sayHello = function(){
+//   console.log('Привет я ' + this.name + this.surname)
+// };
+
+// user.sayHello();
+// user2.sayHello();
+
+//---------------------------------------------222222 call
+// function sayHello() {
+//   return this.name + " " + this.surname
+// }
+// const nameUser = sayHello.call(user)
+
+// user2.say = sayHello
+// console.log(user);
+// console.log(user2);
+
+// function greet (greeting) {
+//   console.log(greeting + sayHello.call(user));
+// }
+
+// greet("Hello my friend")
+
+//----------------------------------------------33333 apply
+// function sayHello(greeting, gender){
+//   return greeting + " " + gender + this.name + " " + this.surname
+// }
+// console.log(sayHello.call(user, "Hi", "girl "));
+
+// console.log(sayHello.apply(user, ['Sensey', '']));
+
+//------------------------------------------------444 bind
+function sayHello(greeting, gender){
+  return this.name + " " + this.surname
 }
+let a = sayHello.bind(user);
+console.log(a());
 
-console.log(usersArr);
 
-function viewList(data) {
-  // контейнер для сбора елементов что бы вставлять оин раз
-  let appendElem = document.createDocumentFragment();
- 
-  data.forEach(function(item){
-     
-     let card = document.createElement("div")
-      card.classList.add('wrap')
-     
-      let template = `
-          <img class="avatar" src=${item.img}>
-          <div class="content">
-              <h2>name: ${item.name}</h2>
-              <ul>
-                  <li>surname: ${item.surName}</li>
-                  <li>age:${item.age}</li>
-                  <li>gender:${item.gender}</li>
-              </ul>
-          </div>
- 
-      `
-  card.innerHTML = template
-  appendElem.appendChild(card)
-      })
- 
-  document.body.append(appendElem)
+//---------------------------- new
+// function CarFactory(marka1, age2, engine3) {
+//   this.marka = marka1;
+//   this.age = age2;
+//   this.engine = engine3; 
+// }
+// let bmw = new CarFactory('BMW', '1', '3.0');
+// console.log(bmw);
+// console.log(bmw.marka);
+// console.log(bmw.age);
+// console.log(bmw.engine);
+
+//------------------------------------------
+class CarFactory{
+  constructor(model, age){
+    this.model = model;
+    this.age = age
+  }
+
+  getCar(){
+    console.log(this.age + this.model);
+  }
 
 }
+let bmw = new CarFactory('BMW 5', 1);
+console.log(bmw);
 
-viewList(usersArr);s
+//----------------------------------------------
